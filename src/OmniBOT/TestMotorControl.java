eeb8ad81a17ba06c8956e97fdd57bde748ee1496
Gustaf Fjaestad <gustaf@fjaestad.nu>
2014-10-15 09:51:33 +0200

    first commit

--- src/OmniBOT/TestMotorControl.java
+++ src/OmniBOT/TestMotorControl.java
@@ -8,17 +8,29 @@ public class TestMotorControl {
 
 	public static void main(String[] args) {
 		MotorControl mc = new MotorControl("10.0.1.1");
-		Point a = new Point(12,12);
-		Point b = new Point(12,112);
 		
-		while(true){
-			Point delta = mc.getDelta(a, b);
-		}
-		//double[] cartSpeeds = mc.deltaToCartSpeeds(delta);
-		//mc.setSpeedsRobot(cartSpeeds);
+		//Try to draw a rectangle
+		Point delta = new Point(100,0);
+		double[] cartSpeeds = mc.deltaToCartSpeeds(delta);
+		mc.setSpeedsRobot(cartSpeeds);
+		Delay.msDelay(2000);
 		
-		//Delay.msDelay(5000);
-		//mc.closeMotorPorts();
+		delta = new Point(0,-100);
+		cartSpeeds = mc.deltaToCartSpeeds(delta);
+		mc.setSpeedsRobot(cartSpeeds);
+		Delay.msDelay(2000);
+		
+		delta = new Point(-100,0);
+		cartSpeeds = mc.deltaToCartSpeeds(delta);
+		mc.setSpeedsRobot(cartSpeeds);
+		Delay.msDelay(2000);
+		
+		delta = new Point(0,100);
+		cartSpeeds = mc.deltaToCartSpeeds(delta);
+		mc.setSpeedsRobot(cartSpeeds);
+		Delay.msDelay(2000);
+			
+		mc.closeMotorPorts();
 	}
 
 }
